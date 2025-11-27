98a42eaea634565f2a553631660c625905ac576d
Arthur Guillemin <aguillemin291@gmail.com>
2025-11-27 17:04:30 +0100

    add new account class

--- src/Account.php
+++ src/Account.php
@@ -7,10 +7,39 @@ class Account
     private string $name;
     private float $balance;
 
-    public function __construct(string $name)
-    {
-        $this->name = $name;
+    private ?string $email;       
+    private ?int $age;           
+    private ?string $type;        
+
+    public function __construct(
+        string $name,
+        ?string $email = null,
+        ?int $age = null,
+        ?string $type = null
+    ) {
+        $this->guardName($name);
+        $this->guardAge($age);
+
+        $this->name    = $name;
         $this->balance = 0;
+
+        $this->email = $email;
+        $this->age   = $age;
+        $this->type  = $type;
+    }
+
+    private function guardName(string $name): void
+    {
+        if (trim($name) === '') {
+            throw new \InvalidArgumentException("name cannot be empty");
+        }
+    }
+
+    private function guardAge(?int $age): void
+    {
+        if ($age !== null && $age < 0) {
+            throw new \InvalidArgumentException("cannot be empty");
+        }
     }
 
     public function getName(): string
@@ -22,7 +51,6 @@ class Account
     {
         return $this->balance;
     }
-    
     public function addBalance($amount){
         $this->balance += $amount;
         return $this->balance;
@@ -32,5 +60,18 @@ class Account
         $this->balance -= $amount;
         return $this->balance;
     }
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
 
+    public function getAge(): ?int
+    {
+        return $this->age;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
 }
